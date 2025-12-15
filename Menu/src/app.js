7527af5a26968136b970a8d033fd60e970f2ab6f
sladjanapekovic <sladjana.pekovic@student.um.si>
2025-12-15 11:59:18 +0100

    Update app.js

--- Menu/src/app.js
+++ Menu/src/app.js
@@ -9,6 +9,20 @@ app.use(express.json());
 app.get("/health", (req, res) => res.json({ ok: true, service: "menu" }));
 
 const PORT = 6001;
+app.get("/menu", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "Pizza Margherita",
+      price: 9.5
+    },
+    {
+      id: 2,
+      name: "Goveja juha",
+      price: 4.2
+    }
+  ]);
+});
 app.listen(PORT, () => {
   console.log("Menu service running on port " + PORT);
 });
